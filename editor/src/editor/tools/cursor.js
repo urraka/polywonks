5010db65fcb6ea80175e6b84dded6b48e5bf1840
urraka <mariano.cuatrin@gmail.com>
2019-08-02 22:15:03 -0300

    update cursor on view change

--- editor/src/editor/tools/cursor.js
+++ editor/src/editor/tools/cursor.js
@@ -3,7 +3,9 @@ import { Tool } from "./tool.js";
 export class CursorTool extends Tool {
     constructor() {
         super();
+        this.mouseEvent = null;
         this.position = { x: 0, y: 0 };
+        this.onViewChange = this.onViewChange.bind(this);
         this.onMouseDown = this.onMouseDown.bind(this);
         this.onMouseMove = this.onMouseMove.bind(this);
         this.onMouseEnter = this.onMouseEnter.bind(this);
@@ -34,6 +36,7 @@ export class CursorTool extends Tool {
     }
 
     onActivate() {
+        this.editor.view.on("change", this.onViewChange);
         this.editor.element.addEventListener("mousedown", this.onMouseDown, true);
         this.editor.element.addEventListener("mousemove", this.onMouseMove, true);
         this.editor.element.addEventListener("mouseenter", this.onMouseEnter);
@@ -41,10 +44,17 @@ export class CursorTool extends Tool {
     }
 
     onDeactivate() {
+        this.editor.view.off("change", this.onViewChange);
         this.editor.element.removeEventListener("mousedown", this.onMouseDown, true);
         this.editor.element.removeEventListener("mousemove", this.onMouseMove, true);
         this.editor.element.removeEventListener("mouseenter", this.onMouseEnter);
         this.editor.element.removeEventListener("mouseleave", this.onMouseLeave);
+    }
+
+    onViewChange() {
+        if (this.mouseEvent) {
+            this.updatePosition(this.mouseEvent);
+        }
     }
 
     onMouseEnter() {
@@ -52,19 +62,22 @@ export class CursorTool extends Tool {
     }
 
     onMouseLeave() {
+        this.mouseEvent = false;
         this.active = false;
     }
 
     onMouseDown(event) {
+        this.mouseEvent = event;
         this.updatePosition(event);
     }
 
     onMouseMove(event) {
+        this.mouseEvent = event;
         this.updatePosition(event);
     }
 
     updatePosition(event) {
-        const rect = event.target.getBoundingClientRect();
+        const rect = this.editor.element.getBoundingClientRect();
         const pos = this.editor.view.canvasToMap(event.clientX - rect.left, event.clientY - rect.top);
         if (pos.x !== this.position.x || pos.y !== this.position.y) {
             this.position.x = pos.x;
